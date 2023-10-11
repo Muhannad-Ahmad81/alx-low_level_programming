78f86ea04ee1d07848fb3845aff48fd1ecd3a966
Muhannad <mohanadahmed.ma21@gmail.com>
2023-10-11 11:44:00 -0700

    l

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - ldkfkknb kfn
+ * desc: sdds sdds gnt
+ * Return:return 0
+ */
+void print_alphabet_x10(void)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		for (int k = 'a'; k <= 'z'; k++)
+		{
+			_putchar(k);
+		}
+	}
+	_putchar('\n');
+}
