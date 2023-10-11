de870e229fa6c483f6b82dc3e0e0642c5d2fe3ac
Muhannad <mohanadahmed.ma21@gmail.com>
2023-10-11 13:08:39 -0700

    j

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,21 +7,20 @@
  */
 
 void print_times_table(int n)
-{	
+{
+	int i;
+	int j;
+
 	if (n < 0 || n > 15)
 	{
 		return;
 	}
 
-	int i;
-	int j;
-	int k;
-
 	for (i = 0; i <= n ; i++)
 	{
 		for (j = 0 ; j <= n ; j++)
 		{
-			_putchar(i*j);
+			_putchar(i * j);
 			_putchar(',');
 			_putchar(' ');
 		}
