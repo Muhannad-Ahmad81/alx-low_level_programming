1252b6da5aadb53ebcee893e8d4d6053778d8e1f
Muhannad <mohanadahmed.ma21@gmail.com>
2023-10-11 06:51:29 -0700

    n

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+*print_alphabet - sjsb jff ff
+*@n: assc eer erb
+*slkvlvrvek df pdd
+*/
+void int print_sign(int n)
+{
+if (n == 0)
+{
+	_putchar('0');
+	return (0);
+}
+else if (n > 0)
+{
+	_putchar('+');
+	return (1);
+}
+else
+{
+	_putchar('-');
+	return (-1);
+}
+}
