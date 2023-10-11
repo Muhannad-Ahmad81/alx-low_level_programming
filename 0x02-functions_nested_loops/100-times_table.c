69af891cce36a3eae5ebd9b169e0fe329e773dd0
Muhannad <mohanadahmed.ma21@gmail.com>
2023-10-11 13:02:19 -0700

    j

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,46 +7,24 @@
  */
 
 void print_times_table(int n)
-{
-
-int a = 0, rep, b;
-
-if (n < 0 || n > 15)
-	return;
-
-while (a <= n)
-{
-	for (b = 0; b <= n; b++)
+{	
+	if (n < 0 || n > 15)
 	{
-		rep = a * b;
-		if (b == 0)
-			_putchar('0' + rep);
-		else if (rep < 10)
+		return;
+	}
+
+	int i;
+	int j
+	int k;
+
+	for (i = 0; i <= n ; i++)
+	{
+		for (j = 0 ; j <= n ; j++)
 		{
-			_putchar(' ');
-			_putchar(' ');
-			_putchar('0' + rep);
-		}
-		else if (rep < 100)
-		{
-			_putchar(' ');
-			_putchar('0' + rep / 10);
-			_putchar('0' + rep % 10);
-		}
-		else
-		{
-			_putchar('0' + rep / 100);
-			_putchar('0' + (rep - 100) / 10);
-			_putchar('0' + rep % 10);
-		}
-		if (b < n)
-		{
+			_putchar(i*j);
 			_putchar(',');
 			_putchar(' ');
 		}
+		_puchar('\n');
 	}
-	_putchar('\n');
-	a++;
 }
-}
-
