1275ba652909ba3f1bda5cc9b3513764d5dea1cd
Muhannad <mohanadahmed.ma21@gmail.com>
2023-10-25 14:30:15 -0700

    v

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{	
+	_puts_recursion(char *s)
+	{
+		printf("%s",s);
+		printf("\n");
+	}
+	return (0);
+}
